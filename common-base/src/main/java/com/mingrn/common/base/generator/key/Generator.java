4e248304332e33d16a0a389dfa8d53af33c017c8
MinGRn <MinGRn97@gmail.com>
2018-10-30 14:28:00 +0800

    新增:Key生成策略工厂;

---
+++ common-base/src/main/java/com/mingrn/common/base/generator/key/Generator.java
@@ -0,0 +1,16 @@
+package com.mingrn.common.base.generator.key;
+
+/**
+ * id生成器接口
+ *
+ * @author MinGRn
+ */
+public interface Generator {
+
+	/**
+	 * Id 生成
+	 *
+	 * @return String id
+	 */
+	String generateID();
+}
